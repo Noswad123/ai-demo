464309098854f570999c691ae2b5ed9d15249be9
Jamal Dawson <Jamal.Dawson.@modernatx.com>
2024-04-30 10:27:18 -0400

    fix: base

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ChatGpt and Co-pilot demo
+
+## setup
+
+Run at the root of this project:
+
+```bash
+npm i --g .
+```
---
+++ bin/index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import chalk from 'chalk';
+
+const usage = chalk.green("\nUsage: ai-demo -c <command>");
+
+const options = yargs(hideBin(process.argv))
+  .usage(usage)
+  .option("c", {
+    alias: "command",
+    describe: "Command to run",
+    type: "string",
+    demandOption: false
+  })
+  .help(true)
+  .argv;
+
+function runCommand(command) {
+  console.log(chalk.blue("Running command: ", command));
+
+}
+
+if (options.c) {
+  runCommand(options.c);
+} else {
+    console.log(chalk.red("Both sentence and language must be specified."));
+}
